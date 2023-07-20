60c72a5d01a97a6d46c089b29d8b63520261a67c
codetilda <107616812+codetilda@users.noreply.github.com>
2023-07-20 18:48:41 +0200

    block number, block transactions

--- src/App.js
+++ src/App.js
@@ -16,11 +16,22 @@ const settings = {
 // Alchemy SDK is an umbrella library with several different packages.
 //
 // You can read more about the packages here:
-//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
+// https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
 const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [loading, setLoading] = useState(false)
+  const [block, setBlock] = useState({});
+  
+  async function getTransactions(blockNumber) {
+    console.log('With transaction details')
+    setLoading(true);
+    setBlock(await alchemy.core.getBlockWithTransactions(blockNumber));
+    setLoading(false);
+    console.log(await alchemy.core.getBlockWithTransactions(blockNumber));
+    // this function returns block with the transaction and their details.
+  }
 
   useEffect(() => {
     async function getBlockNumber() {
@@ -28,9 +39,40 @@ function App() {
     }
 
     getBlockNumber();
-  });
+    // getBlock();
+    getTransactions();
+  }, []);
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  return <>
+    {loading ? (<h1 id='loading'>Loading....</h1>) : (
+      <>
+        <h1 id='heading'>Welcome to Block Explorer!</h1>
+        <div className='container'>
+          <h1>Block# {blockNumber}</h1>
+          <p>{String(new Date(block.timestamp * 1000))}</p>
+          <p>Block Number: {blockNumber}</p>
+          <p>Block Hash: {block.hash}</p>
+          <p>Block Miner: {block.miner}</p>
+          <p>Nonce: {parseInt(block.nonce)}</p>
+          <p>Timestamp: {block.timestamp}</p>
+          <p>Difficulty: {block.difficulty}</p>
+        </div>
+        <div className='container'>
+          <h1>Transactions</h1>
+          {(JSON.stringify(block) === '{}') ? (<h1>Loading...</h1>) : (
+            block.transactions.map((transaction) => {
+              const { from, to, nonce, extraData } = transaction;
+              return <section id='transaction' key={extraData}>
+                <p>From: {from}</p>
+                <p>To: {to}</p>
+                <p>Nonce: {nonce}</p>
+              </section>
+            })
+          )}
+        </div>
+      </>
+    )}
+  </>;
 }
 
 export default App;
